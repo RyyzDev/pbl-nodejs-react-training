63c32d5bc2236c144b4ee788012b046bebfed93d
RyyzDev <fachriakbarkutubi@gmail.com>
2026-08-03 10:42:33 +0700

    kontrol alur

--- Basic Web/JavaScript/kontrolAlur.js
+++ Basic Web/JavaScript/kontrolAlur.js
@@ -1,5 +1,6 @@
 const nilai = 75;
 
+// If else statement
 if (nilai >= 90) {
     console.log("Nilai A");
 }else if (nilai >= 80) {
@@ -12,17 +13,18 @@ if (nilai >= 90) {
     console.log("Nilai E");
 }
 
+// Switch statement
 switch (nilai) {
-    case 90:
+    case nilai>= 90:
         console.log("Nilai A");
         break;
-    case 80:
+    case nilai >= 80:
         console.log("Nilai B");
         break;
-    case 70:
+    case nilai >= 70:
         console.log("Nilai C");
         break;
-    case 60:
+    case nilai >= 60:
         console.log("Nilai D");
         break;
     default:
