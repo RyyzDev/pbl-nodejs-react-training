bf132f388de427b0a784884b1a98070fe71f6250
RyyzDev <fachriakbarkutubi@gmail.com>
2026-08-04 09:46:28 +0700

    change static to conditional

--- Basic Web/JavaScript/Basic/latihan/belanjaDiskon.js
+++ Basic Web/JavaScript/Basic/latihan/belanjaDiskon.js
@@ -2,7 +2,9 @@
 let headset = 225000;
 let mouse = 150000;
 let keyboard = 350000;
+let bayar = 800000;
 
+let total = headset + mouse + keyboard;
 
 console.log("===== Rincian Pembelian =====");
 console.log("Headset: ", headset);
@@ -10,25 +12,23 @@ console.log("Mouse: ", mouse);
 console.log("Keyboard :", keyboard);
 console.log("");
 console.log("");
-
-// Total belanja
-let total = headset + mouse + keyboard;
 console.log("Total Belanja: ", total);
 
-// Deklarasi Diskon
-let diskon = 0.1;
-console.log("Diskon: ", diskon*100, "%");
 
+let diskon;
+if (total >= 800000){
+    diskon = 0.15;
+}else if (total >= 500000){
+    diskon = 0.10;
+}else if (total >= 250000){
+    diskon = 0.05;
+}
 
-// Harga setelah potongan diskon
-let potongan = total * 0.1;
+let potongan = total * diskon;
 total = total - potongan;
-console.log("Total Setelah Diskon: Rp.", total);
-
-// Tampilkan Uang Bayar
-let bayar = 800000;
-console.log("Pembayaran: ", bayar);
-
-// Tampilkan kembalian
 let kembalian = bayar - total;
+
+console.log("Diskon: ", diskon*100, "%");
+console.log("Total Setelah Diskon: Rp.", total);
+console.log("Pembayaran: ", bayar);
 console.log("Kembalian: ", kembalian);
