4569e0a8a96387fa19215e5a250e97cca393ba64
RyyzDev <fachriakbarkutubi@gmail.com>
2026-08-04 16:04:28 +0700

    new file:   Basic Web/JavaScript/Basic/try-catch.js

---
+++ Basic Web/JavaScript/Basic/try-catch.js
@@ -0,0 +1,10 @@
+// tanpa try-catch, program akan langsung berhenti jika ada error
+// dengan try-catch program tetap dijalankan walaupun error
+console.log("Program Mulai!")
+try {
+    let hasil = 10/5;
+    console.log(nama);
+} catch(error) {
+    console.log("Program Error: ", error);
+}
+console.log("Program tetap berjalan!")
