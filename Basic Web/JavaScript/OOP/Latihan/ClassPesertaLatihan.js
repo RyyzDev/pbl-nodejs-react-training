0bc7ee3d9433733e62eff5aae36e0387549c04e1
RyyzDev <fachriakbarkutubi@gmail.com>
2026-08-05 14:32:57 +0700

    latihan getter setter

--- Basic Web/JavaScript/OOP/Latihan/ClassPesertaLatihan.js
+++ Basic Web/JavaScript/OOP/Latihan/ClassPesertaLatihan.js
@@ -5,25 +5,42 @@ class Peserta {
         this.nama = nama;
         this.umur = umur;
         this.proglat = proglat;
-        this.nilai = nilai;
+        this._nilai = nilai; // mengguanakn private funtion
+    }
+
+    // note: nama setter tidak boleh sama dengan nama properti bagian (this)
+    get nilai(){
+        if (this._nilai != undefined){
+            return this._nilai; // jika nilai sudah di set maka kembalikan nilai
+        }else {
+            return "Nilai belum di set!!"; // jika nilai belum di set atay undefined maka kembalikan peringatan
+        }
+    }
+
+    set nilai(x){
+        if (x >= 0 && x <= 100){
+            this._nilai = x; // jika range nilai yang diinput melebihi batas 0 <= x <= 100 maka assign nilai x
+        } else {
+            console.log("Input nilai tidak valid"); // jika range nilai yang diinput tidak di dalam batas 0 <= x <= 100 maka kembalikan peringatan
+        }
     }
 
     grade(){
         switch(true){
             // jika nilai diatas sama dengan 90
-            case (this.nilai >= 90):
+            case (this._nilai >= 90):
                 return 'Sangat Baik';
                 break;
             // jika nilai diatas sama dengan 80
-            case (this.nilai >= 80):
+            case (this._nilai >= 80):
                 return 'Baik';
                 break;
             // jika nilai diatas sama dengan 70
-            case (this.nilai >= 70):
+            case (this._nilai >= 70):
                 return 'Cukup';
                 break;
             // jika nilai diatas sama dengan 60
-            case (this.nilai >= 60):
+            case (this._nilai >= 60):
                 return 'Kurang';
                 break;
             // jika nilai tidak masuk kondisi apapun
@@ -37,8 +54,14 @@ class Peserta {
 const peserta1 = new Peserta('Ryyz', 11, 'web');
 console.log(`Halo, Nama saya ${peserta1.nama}, umur saya ${peserta1.umur}, saya ikut program latihan ${peserta1.proglat}`);
 
+//getter dan setter peserta1
+//setter
+peserta1.nilai = 150;
+//getter
+console.log(peserta1.nilai); 
+
 // instance kedua dari class peserta
-const peserta2 = new Peserta('John', 22, 'tik', 80);
+const peserta2 = new Peserta('John', 22, 'tik', 150);
 console.log(`Halo, Nama saya ${peserta2.nama}, umur saya ${peserta2.umur}, saya ikut program latihan ${peserta2.proglat}, Grade saya ${peserta2.grade()}`);
 
 // instance ketiga dari class peserta
